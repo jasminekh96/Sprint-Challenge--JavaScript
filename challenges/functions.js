85495363f9af65c0852b8f6b994a499811588e9d
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-09-20 12:46:47 -0400

    prototypeComplete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume(a, b, callback) {
+	return callback(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -13,16 +16,31 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+	return a + b;
+}
+
+// consume(2, 2, function(add) {
+// 	console.log(add);
+// });
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function greeting(first, last) {
+	return `Hello ${first} ${last}, nice to meet you!`;
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume('Mary', 'Poppins', greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation:
+// A closure is identified in a different area but we can access it whenever we need it. The nested functions allows us to identify and hold onto that code to access later on. A nested function is also lexical. The nested function gives access to the parent element which gives access to the global scope.
 
 const external = "I'm outside the function";
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -157,7 +157,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
-zooAnimals.map(lowercase) => {
+zooAnimals.map((lowercase) => {
 	return lowCaseAnimalNames.push(`${lowercase.animal_name.toLowerCase()}`);
 });
 console.log(lowCaseAnimalNames);
